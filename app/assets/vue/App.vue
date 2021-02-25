d302f26e597084e86ccdb964b6df6dac7bffc2f6
ilyadyadkov <ilya.dyadkov@gmail.com>
2021-02-25 19:04:53 +0300

    #add vuejs structure

---
+++ app/assets/vue/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link
+        class-name="navbar-brand"
+        to="/home"
+      >
+        App
+      </router-link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div
+        id="navbarNav"
+        className="collapse navbar-collapse"
+      >
+        <ul className="navbar-nav">
+          <router-link
+            class-name="nav-item"
+            tag="li"
+            to="/home"
+            active-class="active"
+          >
+            <a className="nav-link">Home</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+}
+</script>
