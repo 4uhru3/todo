80494efcf677b0a4c9253fe9b0435a5cf48aef41
ilyadyadkov <ilya.dyadkov@gmail.com>
2021-02-28 03:04:07 +0300

    #enable Vue routing
     - update index

--- app/src/Controller/IndexController.php
+++ app/src/Controller/IndexController.php
@@ -11,11 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 final class IndexController extends AbstractController
 {
 	/**
-	 * @Route("/{vueRouting}", name="index")
+	 * @Route@Route("/{vueRouting}", requirements={"vueRouting"="^(?!api|_(profiler|wdt)).*"}, name="index")
 	 * @return Response
 	 */
 	public function indexAction(): Response
 	{
 		return $this->render('base.html.twig', []);
 	}
+
 }
